ae5bcd7ea279a2f0e29dbfa6b8f0e23c42bc4a61
Voidoffi <67222685+Voidoffi@users.noreply.github.com>
2020-11-19 08:02:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import discord, os
 client = discord.Client()
-autorizedchannel = [735505544957394984]
+
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 @client.event
 async def on_message(message):
-    if message.channel.id in autorizedchannel :
+    if message.channel.id == 735505544957394984 :
         await message.delete()
 
 client.run(os.environ['CLEANER'])        
